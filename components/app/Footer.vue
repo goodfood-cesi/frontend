590457021646cfcf082f1a84a1ebee6d2e58cd46
Jules Sayer <jules.sayer@protonmail.com>
2022-01-17 16:48:19 +0100

    feat(app): edited Footer

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -2,9 +2,10 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        <strong>GoodFood</strong> &copy; 2022 | Développé par LORNET &copy;.
+      </p>
+      <p>
+        <RouterLink to="/about">A propos</RouterLink> | <RouterLink to="/contact">Contactez-nous</RouterLink>
       </p>
     </div>
   </footer>
