25a571f08ad6cd9d5ea5b5b55d0cb978cf1cd617
Jules Sayer <jules.sayer@protonmail.com>
2022-01-23 17:28:46 +0100

    fix(navbar): fixed navbar

--- components/app/Navbar.vue
+++ components/app/Navbar.vue
@@ -1,32 +1,20 @@
 <template>
-  <b-navbar class="is-light">
+  <b-navbar class="is-primary">
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
         <img src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png" alt="Lightweight UI components for Vue.js based on Bulma">
       </b-navbar-item>
     </template>
     <template #start>
-      <b-navbar-item href="#">
-        Accueil
-      </b-navbar-item>
-      <b-navbar-item href="#">
+      <b-navbar-item tag="router-link" :to="{ path: '/restaurants' }">
         Commander
       </b-navbar-item>
-      <b-navbar-dropdown label="Plus d'infos">
-        <b-navbar-item href="#">
-          A propos
-        </b-navbar-item>
-        <b-navbar-item href="#">
-          Contact
-        </b-navbar-item>
-      </b-navbar-dropdown>
     </template>
-
     <template #end>
       <b-navbar-item tag="div">
         <div class="buttons">
-          <a class="button is-primary"><b>Inscription</b></a>
-          <a class="button is-light">Connexion</a>
+          <NuxtLink to="/register" class="button is-primary"><b>Inscription</b></NuxtLink>
+          <NuxtLink to="/login" class="button is-light">Connexion</NuxtLink>
         </div>
       </b-navbar-item>
     </template>
