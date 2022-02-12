70e86079f4e080368d97ead8e454e92e28a9beba
Jules Sayer <jules.sayer@protonmail.com>
2022-02-12 18:52:28 +0100

    feat(app): added loading on restaurant page

--- pages/restaurants/_id.vue
+++ pages/restaurants/_id.vue
@@ -1,11 +1,9 @@
 <template>
   <section>
-    <div v-if="restaurant != null" class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <p>{{ restaurant.id }} - {{ restaurant.name }}</p>
-          <p>{{ restaurant.address }}</p>
-        </div>
+    <div class="container is-fluid mt-2 mb-2">
+      <div ref='restaurant' class='box'>
+        <p>{{ restaurant.id }} - {{ restaurant.name }}</p>
+        <p>{{ restaurant.address }}</p>
       </div>
     </div>
   </section>
@@ -15,18 +13,28 @@ export default {
   name: 'RestaurantPage',
   data() {
     return {
-      restaurant: null,
+      restaurant: {
+        id: '',
+        name: '',
+        address: ''
+      }
     }
   },
   async mounted() {
+    const loadingComponent = this.$buefy.loading.open({
+      container: this.$refs.restaurant.$el
+    })
     await this.$axios
       .get('/api/restaurants/restaurants/' + this.$route.params.id)
       .then((response) => {
         this.restaurant = response.data.data
+        loadingComponent.close()
       })
       .catch(() => {
+        loadingComponent.close()
+        this.$router.push('/')
         this.$buefy.snackbar.open({
-          message: 'Impossible de récupérer la liste des restaurants',
+          message: 'Impossible de récupérer les donénes du restaurant',
           type: 'is-danger',
         })
       })
