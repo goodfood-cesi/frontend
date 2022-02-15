bdf980431e33408c38ac3d44b0709f6b816c70bf
Jules Sayer <jules.sayer@protonmail.com>
2022-02-15 16:38:19 +0100

    feat(account): added change password form submit

--- pages/account/password.vue
+++ pages/account/password.vue
@@ -30,7 +30,43 @@
         </div>
         <div class='column'>
           <div class='box'>
-            Modifier mon mot de passe
+            <h3 class='is-size-3'>Modifier mon mot de passe</h3>
+            <form @submit.prevent="submit">
+              <div class='columns is-multiline'>
+                <div class='column is-6'>
+                  <b-field label="Nouveau mot de passe">
+                    <b-input
+                      v-model="password"
+                      minlength="6"
+                      placeholder="*******"
+                      type="password"
+                      password-reveal
+                      required
+                    ></b-input>
+                  </b-field>
+                </div>
+                <div class='column is-6'>
+                  <b-field label="Confirmation">
+                    <b-input
+                      v-model="password_confirmation"
+                      minlength="6"
+                      placeholder="*******"
+                      type="password"
+                      password-reveal
+                      required
+                    ></b-input>
+                  </b-field>
+                </div>
+              </div>
+              <div class="field is-grouped is-grouped-right">
+                <p class="control">
+                  <b-button type="is-primary" native-type="submit">Sauvegarder</b-button>
+                </p>
+                <p class="control">
+                  <b-button type="is-light" @click.prevent='cancel'>Annuler</b-button>
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -41,5 +77,38 @@
 export default {
   name: 'PasswordPage',
   middleware: 'auth',
+  data() {
+    return {
+      password: '',
+      password_confirmation: ''
+    }
+  },
+  methods: {
+    submit() {
+      this.$axios.$post('/api/auth/password', {
+        password: this.password,
+        password_confirmation: this.password_confirmation
+      }).then(() => {
+        this.$buefy.snackbar.open({
+          message: 'Votre mot de passe a été modifié avec succès. Vous avez été déconnecté.',
+          type: 'is-success'
+        })
+        this.$auth.logout()
+      }).catch(() => {
+        this.$buefy.snackbar.open({
+          message: 'Une erreur est survenue lors de la modification de votre mot de passe.',
+          type: 'is-danger'
+        })
+      })
+    },
+    cancel() {
+      this.password = ''
+      this.password_confirmation = ''
+      this.$buefy.snackbar.open({
+        message: 'Vos modifications ont été annulées',
+      })
+    }
+  }
+
 }
 </script>
