06203ae4ca480bcdca1f569df078dfe87abcbf8d
Jules Sayer <jules.sayer@protonmail.com>
2022-02-15 14:42:53 +0100

    feat(account): added account page

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,8 +1,45 @@
 <template>
-  <div>Account page</div>
+  <section>
+    <div class='container is-fluid mt-2 mb-2'>
+      <div class='columns'>
+        <div class='column is-2'>
+          <div class='box'>
+            <aside class="menu">
+              <p class="menu-label">
+                Compte
+              </p>
+              <ul class="menu-list">
+                <li><NuxtLink to='/account' class='is-active'>Mon profil</NuxtLink></li>
+              </ul>
+              <p class="menu-label">
+                Commandes
+              </p>
+              <ul class="menu-list">
+                <li><NuxtLink to='/orders'>Mes commandes</NuxtLink></li>
+                <li><a>Paiements</a></li>
+              </ul>
+              <p class="menu-label">
+                Sécurité
+              </p>
+              <ul class="menu-list">
+                <li><NuxtLink to='/account/password'>Modifier mon mot de passe</NuxtLink></li>
+                <li><a>Supprimer mon compte</a></li>
+              </ul>
+            </aside>
+          </div>
+        </div>
+        <div class='column'>
+          <div class='box'>
+            Modifier mon compte
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   name: 'AccountPage',
+  middleware: 'auth',
 }
 </script>
