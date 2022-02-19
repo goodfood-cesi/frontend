ebbfad3ffac9268d3263578007e47b8f9abd4621
Jules Sayer <jules.sayer@protonmail.com>
2022-02-19 14:15:59 +0100

    feat(cart): added cart on menu

--- components/restaurants/Menu.vue
+++ components/restaurants/Menu.vue
@@ -34,9 +34,43 @@
       <p>Chargement...</p>
     </div>
     <div class='box menu'>
-      <p class="menu-label">
-        Votre panier (0)
-      </p>
+      <div class="menu-label">
+        Votre panier ({{ cart.length }})
+      </div>
+      <ul v-if='cart.length === 0' class="menu-list mb-3">
+        <li class='box'>
+          Votre panier est vide...
+        </li>
+      </ul>
+      <ul v-else class='menu-list mb-3'>
+        <li v-for='item in cart' :key='`cart_` + item.id' class='box is-flex is-justify-content-space-between mb-1'>
+          <div class='is-flex'>
+            <img :src="item.image" alt="" class="image is-32x32" />
+            <p class='ml-1'>{{ item.name }}</p>
+          </div>
+          <span class='tag is-primary'>1x {{ item.amount }} €
+            <b-button
+              class='delete'
+              type='is-primary'
+              @click="$emit('removeItemFromCart', item)"
+            />
+          </span>
+        </li>
+        <li class='box mt-2 is-flex is-justify-content-space-between'>
+          <p>TOTAL :</p>
+          <p><strong>{{ cartTotal | toCurrency }}</strong></p>
+        </li>
+      </ul>
+      <b-button
+        tag="nuxt-link"
+        :to="`/cart`"
+        icon-right="arrow-right-thin-circle-outline"
+        type="is-primary is-light"
+        :disabled="cart.length === 0"
+        expanded
+      >
+        Commander
+      </b-button>
     </div>
   </aside>
 </template>
@@ -53,10 +87,19 @@ export default {
       type: String,
       required: true,
     },
+    cart: {
+      type: Array,
+      required: true,
+    }
   },
   data() {
     return {
       restaurant: null,
+    }
+  },
+  computed: {
+    cartTotal() {
+      return this.cart.reduce((acc, item) => acc + Number(item.amount), 0)
     }
   },
   async mounted() {
