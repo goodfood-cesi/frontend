ba6514fef6f2d1bf044381a7ecf3f9ba9f338bd4
Jules Sayer <jules.sayer@protonmail.com>
2022-02-26 16:09:34 +0100

    feat(menu): added card to menu and total items in cart

--- components/restaurants/Menu.vue
+++ components/restaurants/Menu.vue
@@ -1,82 +1,88 @@
 <template>
   <aside>
-    <div v-if="restaurant != null" class="box menu">
-      <p class="menu-label">
-        {{ restaurant.name }}
-      </p>
-      <ul class="menu-list">
-        <li>
-          <NuxtLink
-            :to="`/restaurants/` + restaurant.id"
-            :class="{
+    <div v-if="restaurant != null" class="card">
+      <div class="card-header">
+        <p class="card-header-title">
+          <NuxtLink :to='`/restaurants/` + restaurant.id'>{{ restaurant.name }}</NuxtLink>
+        </p>
+      </div>
+      <div class='card-content'>
+        <div class='menu'>
+          <ul class="menu-list">
+            <li>
+              <NuxtLink
+                :to="`/restaurants/` + restaurant.id"
+                :class="{
               'is-active': currentPageName === 'restaurants-restaurant',
             }"
-            >Le restaurant
-          </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink
-            :to="`/restaurants/` + restaurant.id + `/menus`"
-            :class="{
+              >Le restaurant
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink
+                :to="`/restaurants/` + restaurant.id + `/menus`"
+                :class="{
               'is-active': currentPageName === 'restaurants-restaurant-menus',
             }"
-          >
-            Menus
-          </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink
-            :to="`/restaurants/` + restaurant.id + `/products`"
-            :class="{
+              >
+                Menus
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink
+                :to="`/restaurants/` + restaurant.id + `/products`"
+                :class="{
               'is-active':
                 currentPageName === 'restaurants-restaurant-products',
             }"
-          >
-            Produits à l'unité
-          </NuxtLink>
-        </li>
-      </ul>
+              >
+                Produits à l'unité
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div v-else class="box">
+    <div v-else class="card">
       <p>Chargement...</p>
     </div>
-    <div class="box menu">
-      <div class="menu-label">Votre panier ({{ cart.length }})</div>
-      <ul v-if="cart.length === 0" class="menu-list mb-3">
-        <li class="box">Votre panier est vide...</li>
-      </ul>
-      <ul v-else class="menu-list mb-3">
-        <li
-          v-for="item in cart"
-          :key="`cart_` + item.type + '_' + item.id"
-          class="box is-flex is-justify-content-space-between mb-1"
-        >
-          <div class="is-flex">
-            <img :src="item.image" alt="" class="image is-32x32" />
-            <p class="ml-1">{{ item.name }}</p>
-          </div>
-          <span class="tag is-primary"
-            >{{ item.quantity }} x {{ item.amount }} €
+    <div class='card mt-2'>
+      <div class='card-header'>
+        <div class="card-header-title">Votre panier ({{ itemsInCart }})</div>
+      </div>
+      <div class='card-content'>
+        <div class="menu">
+          <ul v-if="itemsInCart === 0" class="menu-list mb-3">
+            <li>Votre panier est vide...</li>
+          </ul>
+          <ul v-else class="menu-list mb-3">
+            <li
+              v-for="item in cart"
+              :key="`cart_` + item.type + '_' + item.id"
+              class="is-flex is-justify-content-space-between mb-1"
+            >
+              <div class="is-flex">
+                <img :src="item.image" alt="" class="image is-32x32" />
+                <p class="ml-1">{{ item.name }}</p>
+              </div>
+              <span class="tag is-primary"
+              >{{ item.quantity }} x {{ item.amount }} €
             <b-button
               class="delete"
               type="is-primary"
               @click="$emit('removeItemFromCart', item)"
             />
           </span>
-        </li>
-        <li class="box mt-2 is-flex is-justify-content-space-between">
-          <p>TOTAL :</p>
-          <p>
-            <strong>{{ cartTotal | toCurrency }}</strong>
-          </p>
-        </li>
-      </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
       <footer class="card-footer">
         <div class="card-footer-item">
           <b-button
             icon-right="cart-remove"
             type="is-danger is-light"
-            :disabled="cart.length === 0"
+            :disabled="itemsInCart === 0"
             expanded
             @click="$emit('clearCart')"
           >
@@ -89,10 +95,10 @@
             :to="`/pay`"
             icon-right="arrow-right-thin-circle-outline"
             type="is-primary is-light"
-            :disabled="cart.length === 0"
+            :disabled="itemsInCart === 0"
             expanded
           >
-            Commander
+            Commander ({{ cartTotal | toCurrency }})
           </b-button>
         </div>
       </footer>
@@ -122,6 +128,11 @@ export default {
     currentPageName() {
       return this.$route.name
     },
+    itemsInCart() {
+      return this.cart.reduce((acc, item) => {
+        return acc + item.quantity
+      }, 0)
+    }
   },
 }
 </script>
